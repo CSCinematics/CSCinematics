34cb79323bc0a8e3147bda2b840f89322f1a1e55
CSCinematics <chowdhuryniladri12@gmail.com>
2024-08-31 15:25:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
 var swiper = new Swiper(".mySwiper", {
   scrollbar: {
     el: ".swiper-scrollbar",
